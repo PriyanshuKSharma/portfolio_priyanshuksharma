88a6ea1fdaa05051ae76036181522701d305762b
PriyanshuKSharma <priyanshu17ks@gmail.com>
2025-08-22 23:33:25 +0530

    GitHub Integration Features-Removed-1

---
+++ build.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const ejs = require('ejs');
+
+// Create dist directory
+if (!fs.existsSync('./dist')) {
+  fs.mkdirSync('./dist');
+}
+
+// Copy public folder
+const copyDir = (src, dest) => {
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest, { recursive: true });
+  }
+  const files = fs.readdirSync(src);
+  files.forEach(file => {
+    const srcPath = path.join(src, file);
+    const destPath = path.join(dest, file);
+    if (fs.statSync(srcPath).isDirectory()) {
+      copyDir(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  });
+};
+
+copyDir('./public', './dist');
+
+// Load data
+const projects = JSON.parse(fs.readFileSync('./data/projects.json'));
+const timeline = JSON.parse(fs.readFileSync('./data/timeline.json'));
+
+// Set EJS options
+ejs.renderFile('./views/layout.ejs', {
+  title: 'Home',
+  projects,
+  timeline
+}, (err, html) => {
+  if (err) throw err;
+  fs.writeFileSync('./dist/index.html', html.replace(/\/css\//g, './css/').replace(/\/js\//g, './js/').replace(/\/images\//g, './images/').replace(/\/resume\//g, './resume/'));
+  console.log('Build completed!');
+});
